37aa64199321d4cd3762fc4fcc9e9fb7c6de6e49
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-11 15:30:36 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ console.log('vue ok', Vue)
 const root = new Vue({
     el: '#root',
     data: {
-        title: 'Ciao Classe 68'
+        title: 'Ciao Classe 68',
+        photo :'https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/681/original/Pittogramma-RGB-1080-BLUE-circle.png'
     }
-});
+})
